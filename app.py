12d4e14f982ea358ed948481b3054cc48960e947
Joseph A. Zuercher <jzuercher09@gmail.com>
2024-07-03 05:00:22 -0500

    commit 2

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/js/<path:filename>')
+def serve_js(filename):
+    return app.send_static_file('js/' + filename)
+
+
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
